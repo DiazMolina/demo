2da53a79b4371dbb662aec0b0256bb5f77da401f
cristian diaz <diazmolina1@gmail.com>
2018-08-05 03:45:44 -0500

    xd

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import './ui/klimatic.dart';
+
+void main() async{
+  runApp(new MaterialApp(
+    title: 'klimatic',
+    home: new Klimatic(),
+  ));
+}
---
+++ lib/util/utils.dart
@@ -0,0 +1,2 @@
+final appId = "a78448a807f989c0d157bc8007752bc6";
+final defaultCity = 'London';
